691d23da8b934cd7be9c25ce910e91a862761b06
Abhinav-Gyawali <95914404+Abhinav-Gyawali@users.noreply.github.com>
2025-03-12 09:27:10 +0545

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,3 @@
 echo "BUILD STARTED"
 python3.9 -m pip install -r requirements.txt
 echo "BUILD ENDED"
-echo "MIGRATION STARTED"
-python3.9 manage.py makemigrations --noinput
-echo "MIGRATING"
-python3.9 manage.py migrate --noinput
-echo "COLLECTING STATIC FILES"
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD SUCCESSFUL!"
